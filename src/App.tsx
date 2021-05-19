c578552d9201acd4d9fc229af89eec02659f94e4
jonas-kgomo <jonaskgmoo@gmail.com>
2021-05-19 16:23:30 +0300

    hue fun

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          className="App-logo"
+          src="https://raw.githubusercontent.com/jonas-kgomo/sublinear/main/assets/logo.png"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/dist/App.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+require("./App.css");
+function App() {
+    return (react_1["default"].createElement("div", { className: "App" },
+        react_1["default"].createElement("header", { className: "App-header" },
+            react_1["default"].createElement("img", { className: "App-logo", src: "https://raw.githubusercontent.com/jonas-kgomo/sublinear/main/assets/logo.png" }),
+            react_1["default"].createElement("p", null,
+                "Edit ",
+                react_1["default"].createElement("code", null, "src/App.tsx"),
+                " and save to reload."),
+            react_1["default"].createElement("a", { className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer" }, "Learn React"))));
+}
+exports["default"] = App;
